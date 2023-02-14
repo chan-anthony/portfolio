db95057739dc9c395a68679c89508a7edf16a90f
chan-anthony <92667885+chan-anthony@users.noreply.github.com>
2023-02-14 16:46:56 +0800

    [ADD] Added skills

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+// Animations
 const observer = new IntersectionObserver((entities) => {
   entities.forEach((entity) => {
     if (entity.isIntersecting) {
       entity.target.classList.add('show');
+    } else {
+      entity.target.classList.remove('show');
     }
   });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden,.hidden-right');
 hiddenElements.forEach((element) => observer.observe(element));
+
+// Get current year
+document.getElementById('year').innerHTML = new Date().getFullYear();
